4760d1b4e6fd883f25ae5efc45f4d161a1375715
Bia103 <48888793+Bia103@users.noreply.github.com>
2019-11-14 17:12:00 +0200

    Update Tema_buzzer.ino

--- Lab homework/Tema_buzzer.ino
+++ Lab homework/Tema_buzzer.ino
@@ -7,7 +7,7 @@ int buttonState = 0;
 int ok = 0;
 int buzzerTone = 2500;
 int buzzerValue = 0;
-
+int Do = 262, re = 294 , mi = 330 , fa = 349 , sol = 392 , La = 440 , Si = 494, Do2 = 524;
 unsigned long previousMillis = 0;
 
 void setup() {
@@ -24,16 +24,41 @@ void loop() {
     if (speakerValue != 0)
       Serial.println(speakerValue);
       
-     if (speakerValue > 30) {
+     if (speakerValue > 2) {
       Serial.println("Knock");
       ok = 1;
     }
     if ((currentMillis - previousMillis >= 5000) && ok == 1) {
           previousMillis = currentMillis;
           tone (activeBuzzerPin, buzzerTone);
+        /*  delay(250);
+          tone (activeBuzzerPin, sol);
+          delay(250);
+          tone (activeBuzzerPin, sol);
+          delay(250);
+          tone (activeBuzzerPin, mi);
+          tone (activeBuzzerPin, sol);
+          delay(250);
+          tone (activeBuzzerPin, sol);
+          delay(250);
+          tone (activeBuzzerPin, mi);
+          tone (activeBuzzerPin, sol);
+          delay(250);
+          tone (activeBuzzerPin, sol);
+          delay(250);
+          tone (activeBuzzerPin, fa);
+          delay(250);
+          tone (activeBuzzerPin, re);
+          delay(250);
+          tone (activeBuzzerPin, fa);
+          delay(250);
+          tone (activeBuzzerPin, fa);
+          delay(250);
+          tone (activeBuzzerPin, re);
+          */
   }
   buttonState = digitalRead(pushButton);
-  if(buttonState){
+  if(!buttonState){
     noTone(activeBuzzerPin); 
     buttonState = 0;
   }
